57efaa918d4b21cf39b40aa5a7ef7a2804be5b63
Farhin Mansuri <176534092+MansuriFarhin@users.noreply.github.com>
2026-05-04 16:25:06 -0700

    Updated ESPcode

--- ESP8266/ESPcode/ESPcode.ino
+++ ESP8266/ESPcode/ESPcode.ino
@@ -1,8 +1,8 @@
 #include <ESP8266WiFi.h>
 #include <WiFiUdp.h>    //Library for UDP packets
 
-char ssid[] = "farhin3495"; 
-char pass[] = "Khadijah2021";
+char ssid[] = "SSID"; 
+char pass[] = "PASSWORD";
 
 const unsigned int localPort = 2910; 
 WiFiUDP udp;  
